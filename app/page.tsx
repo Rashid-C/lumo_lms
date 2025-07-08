8ae19447b55f4b648b6090cd91809c23c2c72620
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-07-08 23:28:42 +0530

    Added ThemeToggle for Dark/Light theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from '@/components/ui/themeToggle'
+
 export default function Home() {
   return (
     <div>
       <h1 className='text-2xl fond-bold text-red-500'>Hello Home page</h1>
+      <ThemeToggle />
     </div>
   )
 }
