a573113cd67b9a948fdcd799f93591e68bb45b9c
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-07-09 00:46:55 +0530

    Logo Added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
 import { buttonVariants } from '@/components/ui/button'
 import { ArrowLeft } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { ReactNode } from 'react'
+import Logo from '@/public/logo.svg'
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
@@ -16,7 +18,16 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
         <ArrowLeft className='size-4' />
         Back
       </Link>
-      <div className='flex w-full max-w-sm flex-col gap-6'>{children}</div>
+      <div className='flex w-full max-w-sm flex-col gap-6'>
+        <Link
+          className='flex items-center gap-2 self-center font-medium'
+          href='/'
+        >
+          <Image src={Logo} alt='LOGO' width={32} height={32} />
+          LumoLMS.
+        </Link>
+        {children}
+      </div>
     </div>
   )
 }
